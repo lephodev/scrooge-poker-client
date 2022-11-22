f8e1b01b7cdda8ccc762a06d0d5860b066133c5b
ashrafwbst <er.ashrafkamal@gmail.com>
2022-11-22 13:27:45 +0530

    Update poker logo

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap"
-import logo from "../../assets/game/logo.png";
+import logo from "../../assets/game/logo-poker.png";
 import "./chat.css";
 import { socket } from '../../config/socketConnection';
 
