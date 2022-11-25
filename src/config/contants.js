2049993c074170f1fe00be26ba4de99738c5719d
rizwanwbst <rizwan@websultanate.com>
2022-11-25 15:13:46 +0530

    Add const env

--- src/config/contants.js
+++ src/config/contants.js
@@ -9,8 +9,8 @@ const CONSTANTS = {
 // landingClient: 'http://localhost:3000',
 
 // PRODUCTION ENV
-// landingServerUrl: 'http://localhost:3001',
-// serverUrl: 'http://localhost:3002',
-// landingClient: 'http://localhost:3000',
+// landingServerUrl: 'https://api.scrooge.casino/v1',
+// serverUrl: 'https://poker-api.scrooge.casino',
+// landingClient: 'https://scrooge.casino',
 
 export default CONSTANTS;
