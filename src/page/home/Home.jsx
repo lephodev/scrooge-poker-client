42fe10aec85cffcc1c53edff03c3d8a15d556c6a
ashrafwbst <er.ashrafkamal@gmail.com>
2023-03-04 14:32:14 +0530

    updated lobby design

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -471,38 +471,43 @@ const Home = () => {
     </div>
   );
 };
-
 const customStyles = {
   option: (provided) => ({
     ...provided,
-    background: "#333333",
-    color: "#fff",
+    background: "#000",
+    color: "#ddd",
     fontWeight: "400",
     fontSize: "16px",
-    padding: "12px",
+    padding: "10px 20px",
     lineHeight: "16px",
     cursor: "pointer",
+    borderRadius: "4px",
+    borderBottom: "1px solid #141414",
     ":hover": {
-      background: "#2a2a2a",
+      background: "#141414",
+      borderRadius: "4px",
     },
   }),
   menu: (provided) => ({
     ...provided,
-    background: "#333333",
-    padding: "0px",
+    background: "#000",
+    borderRadius: "30px",
+    padding: "10px 20px",
     border: "2px solid transparent",
   }),
   control: () => ({
-    background: "#333333",
-    border: "2px solid transparent",
-    borderRadius: "4px",
+    background: "#000",
+    border: "2px solid #000",
+    borderRadius: "30px",
     color: "#fff",
     display: "flex",
     alignItem: "center",
-    height: "inherit",
-    margin: "10px 0",
+    height: "41",
+    margin: "2px 0",
+    boxShadow: " 0 2px 10px #000000a5",
+    cursor: "pointer",
     ":hover": {
-      background: "#333333",
+      background: "#000",
       // border: "2px solid #306CFE",
     },
   }),
@@ -522,15 +527,32 @@ const customStyles = {
     fontWeight: "400",
     fontSize: "14px",
     lineHeight: "19px",
-    color: "#fff",
+    color: "#858585c7",
+
   }),
   input: (provided) => ({
     ...provided,
     // height: "38px",
     color: "fff",
+
+  }),
+  valueContainer: (provided) => ({
+    ...provided,
+    padding: "2px 20px",
+  }),
+  indicatorsContainer: (provided) => ({
+    ...provided,
+    paddingRight: "20px",
+    color: '#858585c7',
+  }),
+  svg: (provided) => ({
+    ...provided,
+    fill: '#858585c7 !important',
+    ":hover": {
+      fill: '#858585c7 !important',
+    },
   }),
 };
-
 const CreateTable = ({
   show,
   handleShow,
@@ -549,7 +571,7 @@ const CreateTable = ({
       </Modal.Header>
       <Modal.Body>
         <Form.Group className="form-group" controlId="formPlaintextPassword">
-          <Form.Label>Enter Table name</Form.Label>
+          <Form.Label>Enter Game name</Form.Label>
           <Form.Control
             name="gameName"
             type="text"
