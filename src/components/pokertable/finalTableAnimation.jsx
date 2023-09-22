a36759c6234ecc944115f935709bc9469e39dbf1
akramwbst <akram@websultanate.com>
2023-09-22 17:29:32 +0530

    Worked on final table animation

---
+++ src/components/pokertable/finalTableAnimation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import finalAnimation from "../../assets/animation/fina-table-animation.gif";
+
+const FinalTableAnimation = () => {
+  return (
+    <div className="final-table-animation">
+      <img src={finalAnimation} alt="animation-file" className="img-fluid" />
+    </div>
+  );
+};
+
+export default FinalTableAnimation;
