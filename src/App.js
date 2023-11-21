d7ab4ca5b9ed19537d698ef4a4d370073bcb80ec
akramwbst <akram@websultanate.com>
2023-11-21 11:29:30 +0530

    Added VPN Banner

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ import Error404 from "./page/Error404/Error404";
 import userUtils from "./utils/user";
 import { socket } from "./config/socketConnection";
 import { pokerClient } from "./config/keys";
-import scroogeHat from "./assets/Loader.webp";
+import vpnbanner from "./assets/vpn-banner.webp";
+import notaccess from "./assets/not-access.webp";
 
 import Spectate from "./page/home/spectate";
 import { authInstance } from "./utils/axios.config";
@@ -64,7 +65,7 @@ const App = () => {
         ) {
           toast(
             (t) => (
-              <div className='toaster-join'>
+              <div className="toaster-join">
                 <span>
                   "Tournament will start in 10 seconds, please join the table"
                 </span>
@@ -75,7 +76,8 @@ const App = () => {
                       `${pokerClient}table?gamecollection=poker&tableid=${room?._id}`, //
                       "__self"
                     );
-                  }}>
+                  }}
+                >
                   Join
                 </button>
               </div>
@@ -138,23 +140,26 @@ const App = () => {
   }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       {stateBlock || isVPNEnable ? (
-        <div className='ip-block-content'>
-          <div className='container'>
-            <div className='ip-block-grid'>
-              <img
-                src={scroogeHat}
-                alt='Scrooge Hat'
-                width={96}
-                height={96}
-                loading='lazy'
-              />
-              <p className='ip-block'>
-                {isVPNEnable
-                  ? "VPN detected please turn off the VPN and access again, Thank you!."
-                  : "We are sorry to inform you, but this application is restricted from access in your location"}
-              </p>
+        <div className="ip-block-content">
+          <div className="container">
+            <div className="ip-block-grid">
+              {isVPNEnable ? (
+                <img
+                  src={vpnbanner}
+                  alt="Scrooge VPN"
+                  loading="lazy"
+                  className="img-fluid maintance-img"
+                />
+              ) : (
+                <img
+                  src={notaccess}
+                  alt="Scrooge Access"
+                  loading="lazy"
+                  className="img-fluid maintance-img"
+                />
+              )}
             </div>
           </div>
         </div>
@@ -168,30 +173,31 @@ const App = () => {
               setUser,
               mode,
               setMode,
-            }}>
+            }}
+          >
             <Router>
               <Switch>
-                <Route exact path='/'>
+                <Route exact path="/">
                   <Home />
                 </Route>
-                <Route exact path='/leaderboard'>
+                <Route exact path="/leaderboard">
                   <LeaderBoard />
                 </Route>
-                <Route exact path='/spectate'>
+                <Route exact path="/spectate">
                   <Spectate />
                 </Route>
-                <Route exact path='/table'>
+                <Route exact path="/table">
                   <PokerTable />
                 </Route>
-                <Route path='*'>
+                <Route path="*">
                   <Error404 />
                 </Route>
               </Switch>
             </Router>
           </UserContext.Provider>
-          <div className='abc'>
+          <div className="abc">
             <Toaster
-              position='top-right'
+              position="top-right"
               reverseOrder={false}
               toastOptions={{
                 className: `custom-toast ${customToast ? "cus-toastify" : ""}`,
